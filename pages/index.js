d565fbf4b58fc7b128143f2f4b3365eab8138b7e
mattcroat <mattcroat.dev@gmail.com>
2020-10-23 23:00:04 +0200

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 
 import { getSortedPostsData } from '../lib/posts'
@@ -44,11 +46,13 @@ const Home = ({ allPostsData }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
